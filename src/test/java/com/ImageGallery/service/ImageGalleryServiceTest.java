c591a91262575ec32ee220d16f31c58310d4c9c2
Raunier24 <peplisoft@gmail.com>
2024-07-18 01:26:05 +0200

    test

--- src/test/java/com/ImageGallery/service/ImageGalleryServiceTest.java
+++ src/test/java/com/ImageGallery/service/ImageGalleryServiceTest.java
@@ -29,37 +29,6 @@ public class ImageGalleryServiceTest {
         MockitoAnnotations.openMocks(this);
     }
 
-    /*@Test
-    public void testDeleteImageGallery_WhenImageExists() {
-        int id = 1;
-        when(iImageGalleryRepository.findById(id)).thenReturn(Optional.of(new ImageGallery()));
-        doNothing().when(iImageGalleryRepository).deleteById(id);
-
-        String result = imageGalleryService.deleteImageGallery(id);
-
-        assertEquals("You have deleted the image with ID: " + id, result);
-    }
-
-    @Test
-    public void testDeleteImageGallery_WhenImageDoesNotExist() {
-        int id = 1;
-        when(iImageGalleryRepository.findById(id)).thenReturn(Optional.empty());
-
-        String result = imageGalleryService.deleteImageGallery(id);
-
-        assertEquals("The image with ID: " + id + " does not exist.", result);
-    }
-
-    @Test
-    public void testDeleteImageGallery_WhenExceptionOccurs() {
-        int id = 1;
-        when(iImageGalleryRepository.findById(id)).thenThrow(new RuntimeException("Unexpected error"));
-
-        String result = imageGalleryService.deleteImageGallery(id);
-
-        assertEquals("An unexpected error occurred while trying to delete the image with ID: " + id, result);
-    }*/
-
     @Test
     public void testCreateImageGallery() {
         int id = 123;
