4e89567c98c9491aa5e74d543363379bdf867a3f
JuanMolinaRey <Juanmolinarey7@gmail.com>
2024-07-17 20:18:56 +0200

    update testservice

--- src/test/java/com/ImageGallery/service/ImageGalleryServiceTest.java
+++ src/test/java/com/ImageGallery/service/ImageGalleryServiceTest.java
@@ -1,2 +1,33 @@
 package com.ImageGallery.service;
 
+
+import com.ImageGallery.repository.IImageGalleryRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import static org.mockito.Mockito.*;
+
+public class ImageGalleryServiceTest {
+
+    @Mock
+    private IImageGalleryRepository iImageGalleryRepository;
+
+    @InjectMocks
+    private ImageGalleryService imageGalleryService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testDeleteImageGallery() {
+        int id = 1;
+
+        imageGalleryService.deleteImageGallery(id);
+
+        verify(iImageGalleryRepository).deleteById(id);
+    }
+}
