54c1bdff09603566840584ffce0c49a7aacc4edc
Raunier24 <peplisoft@gmail.com>
2024-07-17 22:44:14 +0200

    Copied from dev package service

--- src/main/java/com/ImageGallery/service/ImageGalleryService.java
+++ src/main/java/com/ImageGallery/service/ImageGalleryService.java
@@ -1,11 +1,9 @@
 package com.ImageGallery.service;
 
 import java.util.ArrayList;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.ImageGallery.model.ImageGallery;
 import com.ImageGallery.repository.IImageGalleryRepository;
 
@@ -15,22 +13,12 @@ public class ImageGalleryService {
     @Autowired
     static IImageGalleryRepository iImageGalleryRepository;
 
-    public String deleteImageGallery(int id) {
-        try {
-            Optional<ImageGallery> imageGallery = iImageGalleryRepository.findById(id);
-            if (imageGallery.isPresent()) {
-                iImageGalleryRepository.deleteById(id);
-                return "You have deleted the image with ID: " + id;
-            } else {
-                return "The image with ID: " + id + " does not exist.";
-            }
-        } catch (Exception e) {
-            return "An unexpected error occurred while trying to delete the image with ID: " + id;
-        }
+    public void deleteImageGallery(int id) {
+        iImageGalleryRepository.deleteById(id);
     }
 
     public void updateImageGallery(ImageGallery image, int id) {
-        image.setId(id);
+        image.setId((long) id);
         iImageGalleryRepository.save(image);
 
     }
@@ -38,19 +26,10 @@ public class ImageGalleryService {
     public ArrayList<ImageGallery> getAllImageGallery() {
         return (ArrayList<ImageGallery>) iImageGalleryRepository.findAll();
     }
+        @Autowired
+        private IImageGalleryRepository imageGalleryRepository;
 
-    @Autowired
-    private IImageGalleryRepository IImageGalleryRepository;
-
-    public static ImageGallery createImageGallery(ImageGallery image) {
-        int id = 0;
-        image.setId(id);
-        String title = null;
-        image.setTitle(title);
-        String description = null;
-        image.setDescription(description);
-        String url = null;
-        image.setUrl(url);
-        return iImageGalleryRepository.save(image);
+        public ImageGallery createImageGallery(ImageGallery image) {
+            return imageGalleryRepository.save(image);
+        }
     }
-}
