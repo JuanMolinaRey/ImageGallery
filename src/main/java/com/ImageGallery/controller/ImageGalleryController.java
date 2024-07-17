94bd4a4208dd3a510093bcb89cc5ec4924968931
JuanMolinaRey <Juanmolinarey7@gmail.com>
2024-07-17 14:58:44 +0200

    update controllers

--- src/main/java/com/ImageGallery/controller/ImageGalleryController.java
+++ src/main/java/com/ImageGallery/controller/ImageGalleryController.java
@@ -1,8 +1,11 @@
 package com.ImageGallery.controller;
 
+import com.ImageGallery.model.ImageGallery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import com.ImageGallery.service.ImageGalleryService;
+
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -13,7 +16,25 @@ public class ImageGalleryController {
     ImageGalleryService imagegalleryService;
 
     @DeleteMapping(path = "/images/{id}")
-     public String deleteImageGallery(@PathVariable int id) {
-          return imagegalleryService.deleteImageGallery( id);
-     }
+    public void deleteImageGallery(@PathVariable int id) {
+        imagegalleryService.deleteImageGallery(id);
+    }
+
+    @PutMapping (path = "/images/{id}")
+    public void updateImageGallery(@RequestBody ImageGallery imageGallery, @PathVariable int id){
+        imagegalleryService.updateImageGallery(imageGallery, id);
+
+    }
+    @GetMapping(path = "/images/{id}")
+    public ArrayList<ImageGallery> getAllImageGallery() {
+        return imagegalleryService.getAllImageGallery();
+    }
+
+    @PostMapping("/createImageGallery")
+    public ImageGallery createImageGallery(@RequestBody ImageGallery image, @RequestParam int id,
+                                           @RequestParam String title, @RequestParam String description,
+                                           @RequestParam String url) {
+        image.setId(id);
+        return ImageGalleryService.createImageGallery(image, id, title, description, url);
+    }
 }
