954a464ee0df514adf5f09ffd437826c77953bdb
JuanMolinaRey <Juanmolinarey7@gmail.com>
2024-07-17 15:00:06 +0200

    update service

--- src/main/java/com/ImageGallery/service/ImageGalleryService.java
+++ src/main/java/com/ImageGallery/service/ImageGalleryService.java
@@ -1,23 +1,42 @@
 package com.ImageGallery.service;
 
+import com.ImageGallery.model.ImageGallery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ImageGallery.repository.IImageGalleryRepository;
 
+import java.util.ArrayList;
+
 @Service
 
 public class ImageGalleryService {
     @Autowired
-    private IImageGalleryRepository iImageGalleryRepository;
+    static IImageGalleryRepository iImageGalleryRepository;
 
-    public String deleteImageGallery(int id) {
-        if (iImageGalleryRepository.existsById(id)) {
-            iImageGalleryRepository.deleteById(id);
-            return "You have deleted the image with ID: " + id;
-        } else {
-            return "No image found with ID: " + id;
-        }
+    public void deleteImageGallery(int id) {
+        iImageGalleryRepository.deleteById(id);
+    }
+
+    public void updateImageGallery(ImageGallery image, int id) {
+        image.setId(id);
+        iImageGalleryRepository.save(image);
+
+    }
+
+    public ArrayList<ImageGallery> getAllImageGallery() {
+        return (ArrayList<ImageGallery>) iImageGalleryRepository.findAll();
+    }
+
+    @Autowired
+    private IImageGalleryRepository IImageGalleryRepository;
+
+    public static ImageGallery createImageGallery(ImageGallery image, int id, String title, String description, String url) {
+        image.setId(id);
+        image.setTitle(title);
+        image.setDescription(description);
+        image.setUrl(url);
+        return iImageGalleryRepository.save(image);
     }
 }
 
