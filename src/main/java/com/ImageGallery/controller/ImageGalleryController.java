9191af7c99cca06104c21a9a56c60e7a2cf03cba
Raunier24 <peplisoft@gmail.com>
2024-07-18 09:10:38 +0200

    controller Image

--- src/main/java/com/ImageGallery/controller/ImageGalleryController.java
+++ src/main/java/com/ImageGallery/controller/ImageGalleryController.java
@@ -1,27 +1,46 @@
 package com.ImageGallery.controller;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.ImageGallery.model.ImageGallery;
 import com.ImageGallery.service.ImageGalleryService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/createImageGallery")
+@RequestMapping("/api/v1")
+@CrossOrigin(origins = "*")
 public class ImageGalleryController {
 
     @Autowired
     private ImageGalleryService imageGalleryService;
 
-    @PostMapping("api/createImageGallery")
-    public ImageGallery createImageGallery(@RequestBody ImageGallery image,
-                                           @RequestParam int id,
-                                           @RequestParam String title,
-                                           @RequestParam String description,
-                                           @RequestParam String url) {
-        image.setId((long) id);
-        image.setTitle(title);
-        image.setDescription(description);
-        image.setUrl(url);
+    @GetMapping
+    public ArrayList<ImageGallery> getAllImageGallery() {
+        return imageGalleryService.getAllImageGallery();
+    }
+
+    @PostMapping(path = "/images")
+    public ImageGallery createImageGallery(@RequestBody ImageGallery image) {
         return imageGalleryService.createImageGallery(image);
+    }
+
+    @PutMapping(path = "/images/{id}")
+    public void updateImageGallery(@RequestBody ImageGallery imageGallery, @PathVariable int id) {
+        imageGalleryService.updateImageGallery(imageGallery, id);
+    }
+
+    @DeleteMapping(path = "/images/{id}")
+    public void deleteImageGallery(@PathVariable int id) {
+        imageGalleryService.deleteImageGallery(id);
     }
 }
