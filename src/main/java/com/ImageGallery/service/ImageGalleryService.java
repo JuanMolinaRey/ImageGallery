48488c81d07d5627f6a5a87436eb5632c9d02226
Raunier24 <peplisoft@gmail.com>
2024-07-18 09:11:50 +0200

    service Image

--- src/main/java/com/ImageGallery/service/ImageGalleryService.java
+++ src/main/java/com/ImageGallery/service/ImageGalleryService.java
@@ -4,32 +4,34 @@ import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import com.ImageGallery.model.ImageGallery;
 import com.ImageGallery.repository.IImageGalleryRepository;
 
 @Service
-
 public class ImageGalleryService {
+
+    private final IImageGalleryRepository iImageGalleryRepository;
+
     @Autowired
-    static IImageGalleryRepository iImageGalleryRepository;
+    public ImageGalleryService(IImageGalleryRepository iImageGalleryRepository) {
+        this.iImageGalleryRepository = iImageGalleryRepository;
+    }
+
+    public ImageGallery createImageGallery(ImageGallery imageGallery) {
+        return iImageGalleryRepository.save(imageGallery);
+    }
 
     public void deleteImageGallery(int id) {
         iImageGalleryRepository.deleteById(id);
     }
 
     public void updateImageGallery(ImageGallery image, int id) {
-        image.setId((long) id);
+        image.setId(id);
         iImageGalleryRepository.save(image);
-
     }
 
     public ArrayList<ImageGallery> getAllImageGallery() {
         return (ArrayList<ImageGallery>) iImageGalleryRepository.findAll();
     }
-        @Autowired
-        private IImageGalleryRepository imageGalleryRepository;
-
-        public ImageGallery createImageGallery(ImageGallery image) {
-            return imageGalleryRepository.save(image);
-        }
-    }
+}
