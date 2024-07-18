20718ac6c04ec949b15f3f22f88d1f4da42653b1
Isamar Romero Bellorin <romerobellorini@gmail.com>
2024-07-18 09:10:22 +0200

    ImageGalleryService.java

--- src/main/java/com/ImageGallery/service/ImageGalleryService.java
+++ src/main/java/com/ImageGallery/service/ImageGalleryService.java
@@ -1,7 +1,7 @@
 package com.ImageGallery.service;
 
 import java.util.ArrayList;
-import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.ImageGallery.model.ImageGallery;
@@ -13,22 +13,12 @@ public class ImageGalleryService {
     @Autowired
     static IImageGalleryRepository iImageGalleryRepository;
 
-    public String deleteImageGallery(int id) {
-        try {
-            Optional<ImageGallery> imageGallery = iImageGalleryRepository.findById(id);
-            if (imageGallery.isPresent()) {
-                iImageGalleryRepository.deleteById(id);
-                return "You have deleted the image with ID: " + id;
-            } else {
-                return "The image with ID: " + id + " does not exist.";
-            }
-        } catch (Exception e) {
-            return "An unexpected error occurred while trying to delete the image with ID: " + id;
-        }
+    public void deleteImageGallery(int id) {
+        iImageGalleryRepository.deleteById(id);
     }
 
     public void updateImageGallery(ImageGallery image, int id) {
-        image.setId(id);
+        image.setId((long) id);
         iImageGalleryRepository.save(image);
 
     }
@@ -38,13 +28,11 @@ public class ImageGalleryService {
     }
 
     @Autowired
-    private IImageGalleryRepository IImageGalleryRepository;
+    private IImageGalleryRepository imageGalleryRepository;
 
-    public static ImageGallery createImageGallery(ImageGallery image, int id, String title, String description, String url) {
-        image.setId(id);
-        image.setTitle(title);
-        image.setDescription(description);
-        image.setUrl(url);
-        return iImageGalleryRepository.save(image);
+    public ImageGallery createImageGallery(ImageGallery image) {
+        return imageGalleryRepository.save(image);
+
     }
 }
+
