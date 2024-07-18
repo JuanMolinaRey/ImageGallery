122d7453bf0a1576fc2ea1b5a4f4b071d124d942
JuanMolinaRey <Juanmolinarey7@gmail.com>
2024-07-18 11:17:15 +0200

    update model

--- src/main/java/com/ImageGallery/model/ImageGallery.java
+++ src/main/java/com/ImageGallery/model/ImageGallery.java
@@ -1,1 +1,33 @@
+package com.ImageGallery.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Table
+public class ImageGallery {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "url")
+    private String url;
+
+}
